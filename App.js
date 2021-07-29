bc832f9c7af5a1d88abc32bddfd464c1c40f76f9
akhilaNarender <akhilavujjini2@gmail.com>
2021-07-29 14:42:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
 import * as React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  Image,
-  Alert
-} from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Image, Alert} from 'react-native';
 import { Header } from 'react-native-elements';
 import db from './localdb';
 import PhonicSoundButton from './components/PhonicSoundButton';
